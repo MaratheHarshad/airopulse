4cef4b7397da231d366cc402129e4b96a3b131e2
Harshad <harshadmarathe8@gmail.com>
2026-01-19 19:50:20 +0530

    Initial codebase setup

---
+++ src/main/java/com/app/airopulse/AiropulseApplication.java
@@ -0,0 +1,13 @@
+package com.app.airopulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiropulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiropulseApplication.class, args);
+	}
+
+}
