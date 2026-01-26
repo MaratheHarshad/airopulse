3cc00e611a01480fffd9b225063473d1ac21ca62
Harshad <harshadmarathe8@gmail.com>
2026-01-26 21:37:01 +0530

    feat(add flight service for state management)

--- src/main/java/com/app/airopulse/controller/FlighController.java
+++ src/main/java/com/app/airopulse/controller/FlighController.java
@@ -1,6 +1,5 @@
-package com.app.airopulse.controllers;
+package com.app.airopulse.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
